b72a861d0c523ab626a5505357fb1603d336011c
Stephen L <LRQ3000@gmail.com>
2023-07-15 23:06:30 +0200

    bump v1.0.4
    Signed-off-by: Stephen L. <LRQ3000@gmail.com>

--- src/py3make/_version.py
+++ src/py3make/_version.py
@@ -9,7 +9,7 @@ from subprocess import STDOUT
 __all__ = ["__version__"]
 
 # major, minor, patch, -extra
-version_info = 1, 0, 3
+version_info = 1, 0, 4
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
